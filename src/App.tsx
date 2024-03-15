aa092edca66107cda2f05982e69ee44cb72b11dd
Davi Silva <davi.pimenta8@gmail.com>
2024-03-15 16:59:20 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,71 @@
-import { useState } from "react"
-import { Todo } from "./types/Todo"
-import Card from "./components/Card"
-import "./App.css"
-
+import { useState } from "react";
+import { Todo } from "./types/Todo";
+import Card from "./components/Card";
+import { KeyboardEvent } from "react";
+import "./App.css";
 
 const App = () => {
- const [todos, setTodos] = useState<Todo[]>([])
- const [value, setValue] = useState("")
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [value, setValue] = useState("");
 
- const addTodo = () => {
-  if (!value) {
-    return;
-  }
-  const allTodos = [...todos, {
-    id: Math.random(),
-    title: value,
-    completed: false
-  }]
-  setTodos(allTodos)
- }
+  const addTodo = () => {
+    if (!value) {
+      return;
+    }
+    const allTodos = [
+      ...todos,
+      {
+        id: Math.random(),
+        title: value,
+        completed: false,
+      },
+    ];
+    setTodos(allTodos);
+  };
 
- const completeTodo = (id: number) => {
-  const allTodos = [...todos]
-  allTodos.map((todo) => todo.id === id ? todo.completed = !todo.completed : todo)
-  setTodos(allTodos)
- }
+  const completeTodo = (id: number) => {
+    const allTodos = [...todos];
+    allTodos.map((todo) =>
+      todo.id === id ? (todo.completed = !todo.completed) : todo
+    );
+    setTodos(allTodos);
+  };
 
- const removeTodo = (id: number) => {
-  const allTodos = [...todos]
-  const filteredTodos = allTodos.filter((todo) => todo.id === id ? null : todo)
-  setTodos(filteredTodos)
- }
+  const removeTodo = (id: number) => {
+    const allTodos = [...todos];
+    const filteredTodos = allTodos.filter((todo) =>
+      todo.id === id ? null : todo
+    );
+    setTodos(filteredTodos);
+  };
+
+  const handleDown = (ev: KeyboardEvent) => {
+    if (ev.key === "Enter") {
+      addTodo();
+    }
+  };
 
   return (
     <div className="app">
       <div className="add-todo">
-        <input type="text" placeholder="Digite uma tarefa para adicionar" onChange={(ev) => setValue(ev.target.value)}
-        value={value}/>
-          <button onClick={addTodo}>Criar tarefa</button>
+        <input
+          type="text"
+          placeholder="Digite uma tarefa para adicionar"
+          onChange={(ev) => setValue(ev.target.value)}
+          value={value}
+          onKeyDown={handleDown}
+        />
+        <button onClick={addTodo}>Criar tarefa</button>
       </div>
-        {todos.map((todo) => (
-         <Card key={todo.id} todoprops={todo} completeTodo = {completeTodo} removeTodo={removeTodo}/>
-        ))}
-      
+      {todos.map((todo) => (
+        <Card
+          key={todo.id}
+          todoprops={todo}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+        />
+      ))}
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
